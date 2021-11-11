9f3532bb4d5610dff1018aff767c1c2b060ea7c8
DESKTOP-NC10JI0\lance <katie-1511@hotmail.com>
2021-11-11 13:36:40 +0800

    Home Component

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,10 +1,31 @@
 import FormExampleForm from "./Form.js";
+import './App.css';
+import {Home} from './Components/Home';
+import {Expenses} from './Components/Expenses';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+
 function App() {
   return (
-    <div className="App">
-      <FormExampleForm />
+    <div className="container">
+    <BrowserRouter>
+      <div> 
+      
+      <Routes>
+     
+
+      <Route path='/' component={FormExampleForm} exact />
+      <Route path='/home' component={Home} />
+      <Route path='/expenses' component={Expenses} />
+      
+    
+      </Routes>
+    
+      </div>
+    </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+

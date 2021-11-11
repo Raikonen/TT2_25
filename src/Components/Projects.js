381f75b77b4144f7a21df207241750d63af2ff65
katiehoql <79960875+katiehoql@users.noreply.github.com>
2021-11-11 17:58:03 +0800

    update projects

---
+++ src/Components/Projects.js
@@ -0,0 +1,86 @@
+import React, {Component} from 'react';
+import {Table} from 'react-bootstrap';
+import {Button, ButtonToolbar, Col} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navigate} from 'react-router-dom';
+
+export class Projects extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state={projs:[], redirectTo: false}
+    }
+
+    componentDidMount(){
+        this.refreshList();
+    }
+
+    refreshList(){
+       fetch('')
+       .then(response => response.json())
+       .then(data => {
+           this.setState({projs: data});
+       });
+    }
+
+    componentDidUpdate(){
+        this.refreshList();
+    }
+
+
+    redirectTo = () =>{
+        this.setState({redirectTo: true})
+    }
+
+    render(){
+    
+    const{projs, projid, projname}=this.state;
+    const redirectTo = this.state.redirectTo;
+        if (redirectTo === true) {
+            return <Navigate to="/expenses" />
+        }
+
+        return(
+        <section class = "content">
+            <div className = "mt-3">
+            <h5>My Projects</h5>
+            </div>
+
+            <Table className="mt-4" striped boredered hover size="sm">
+                <thead>
+                    <tr>
+                        <th>Project Name</th>
+                        <th>Description</th>
+                        <th>Budget</th>
+                        <th></th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {projs.map(proj=>
+                        <tr key={proj.ID}>
+                        <td>{proj.Name}</td>
+                        <td>{proj.Description}</td>
+                        <td>{proj.Budget}</td>
+                        <td>
+                            <ButtonToolbar>
+                                <Col sm={1}></Col>
+                                <Button className="mr-2" variant="primary" 
+                                onClick = {()=> this.redirectTo}
+                                >View</Button>
+                            </ButtonToolbar>
+                        </td>
+                        </tr>
+                        )}
+                </tbody>
+            </Table>
+
+    
+        </section>
+
+        )
+    
+    }
+}
+
+
